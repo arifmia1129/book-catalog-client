1e14e8bbc7100dd01658b8727ac2c7deb224c39d
arifmia1129 <arifmia1129@gmail.com>
2023-08-20 12:12:33 +0600

    provide redux store and make api slice

---
+++ src/redux/api/apiSlice.ts
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-catalog-server-nine.vercel.app/api/v1/",
+  }),
+  endpoints: () => ({}),
+});
+
+export default api;
