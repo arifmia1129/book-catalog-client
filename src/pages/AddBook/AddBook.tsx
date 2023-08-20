087f6decc3aa701bc3bbb7a9d19a41adc82cc933
arifmia1129 <arifmia1129@gmail.com>
2023-08-20 22:42:39 +0600

    add book page added

--- src/pages/AddBook/AddBook.tsx
+++ src/pages/AddBook/AddBook.tsx
@@ -1,3 +1,105 @@
+import { useForm } from "react-hook-form";
+
 export default function AddBook() {
-  return <div>AddBook</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const onSubmit = async (data: object) => {};
+  return (
+    <div className="m-5 max-w-7xl mx-auto">
+      <h3 className="text-2xl font-bold">Add a new book</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid grid-cols-1 ">
+          <div className="form-control w-full md:w-2/3 ">
+            <label className="label">
+              <span className="label-text">Book image url</span>
+            </label>
+            <input
+              {...register("imageUrl")}
+              type="text"
+              placeholder="Write book image url"
+              className="input input-bordered w-full md:w-2/3"
+            />
+          </div>
+          <div className="form-control w-full md:w-2/3">
+            <label className="label">
+              <span className="label-text">Book title *</span>
+            </label>
+            <input
+              {...register("title", { required: "Book title is required" })}
+              type="text"
+              placeholder="Write book title"
+              className="input input-bordered w-full md:w-2/3"
+            />
+            <label className="label">
+              <span className="label-text-alt text-red-500">
+                {errors.title && String(errors.title.message)}
+              </span>
+            </label>
+          </div>
+          <div className="form-control w-full md:w-2/3">
+            <label className="label">
+              <span className="label-text">Book author *</span>
+            </label>
+            <input
+              {...register("author", { required: "Book author is required" })}
+              type="text"
+              placeholder="Write book author"
+              className="input input-bordered w-full md:w-2/3"
+            />
+            <label className="label">
+              <span className="label-text-alt text-red-500">
+                {errors.author && String(errors.author.message)}
+              </span>
+            </label>
+          </div>
+          <div className="form-control w-full md:w-2/3">
+            <label className="label">
+              <span className="label-text">Book genre *</span>
+            </label>
+            <input
+              {...register("genre", { required: "Book genre is required" })}
+              type="text"
+              placeholder="Write book genre"
+              className="input input-bordered w-full md:w-2/3"
+            />
+            <label className="label">
+              <span className="label-text-alt text-red-500">
+                {errors.genre && String(errors.genre.message)}
+              </span>
+            </label>
+          </div>
+          <div className="form-control w-full md:w-2/3">
+            <label className="label">
+              <span className="label-text">Book public date *</span>
+            </label>
+            <input
+              {...register("publicDate", {
+                required: "Book public date is required",
+              })}
+              type="text"
+              placeholder="Write book public date"
+              className="input input-bordered w-full md:w-2/3"
+            />
+            <label className="label">
+              <span className="label-text-alt text-red-500">
+                {errors.publicDate && String(errors.publicDate.message)}
+              </span>
+            </label>
+          </div>
+          <div className=" mt-10">
+            <input
+              className="btn btn-primary text-white w-44"
+              value={"Add"}
+              type="submit"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,21 @@ import App from "../App";
 import Signup from "../pages/Authentication/Signup";
 import Login from "../pages/Authentication/Login";
 import NotFound from "../pages/NotFound/NotFound";
+import AddBook from "../pages/AddBook/AddBook";
+import Book from "../pages/Book/Book";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <App />,
     children: [
       {
         index: true,
-        element: <App />,
+        element: <Book />,
+      },
+      {
+        path: "/add-book",
+        element: <AddBook />,
       },
     ],
   },
