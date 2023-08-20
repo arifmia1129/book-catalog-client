c1e56620e140036e50c8819a49a5f2f1cc7f0ac5
arifmia1129 <arifmia1129@gmail.com>
2023-08-20 19:30:13 +0600

    fetched user profile created by create async thunk to check user access token and login persistant

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { Toaster } from "react-hot-toast";
 import MainLayout from "./layouts/MainLayout";
+import { useAppDispatch } from "./redux/hooks";
+import { useEffect } from "react";
+import { fetchedUserProfile } from "./redux/features/user/userSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchedUserProfile());
+  }, [dispatch]);
+
   return (
     <>
       <MainLayout />
