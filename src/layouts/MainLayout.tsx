47a6473746c48e19ca29be8dd85f92d9eb6a8329
arifmia1129 <arifmia1129@gmail.com>
2023-08-21 10:23:28 +0600

    footer added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../componets/Shared/Navbar";
+import Footer from "../componets/Shared/Footer";
 
 export default function MainLayout() {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
